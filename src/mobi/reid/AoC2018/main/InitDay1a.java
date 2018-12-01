694a27294a6599fa4bb616bfb0e889d8718c6c25
dioxinDev <gavinreid@pseudocoder.co.uk>
2018-12-01 09:15:13 +0100

    End of Day1

--- src/mobi/reid/AoC2018/main/InitDay1a.java
+++ src/mobi/reid/AoC2018/main/InitDay1a.java
@@ -2,16 +2,23 @@ package mobi.reid.AoC2018.main;
 
 import mobi.reid.AoC2018.helper.Helper;
 
+import java.util.StringTokenizer;
 
-/**
- * Created by gavinreid on 28/11/18.
- */
+
 public class InitDay1a {
 
     public static void main(String[] args) {
         String input = Helper.getStringfromFile("./resources/day1ainput.txt");
 
-        System.out.println(input);
+        StringTokenizer tokens = new StringTokenizer(input,"\n");
+        int sum = 0;
+        for(String token; tokens.hasMoreTokens();){
+            token = tokens.nextToken();
+            sum += Integer.parseInt(token);
+        }
+
+
+        System.out.println(sum);
 
     }
 
