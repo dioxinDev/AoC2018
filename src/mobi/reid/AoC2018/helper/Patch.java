e68fe887421862386acdfee1877cc77ef73e0aec
Gavin Reid <gavin@reid.mobi>
2018-12-03 07:50:44 +0100

    Day 3 initial

---
+++ src/mobi/reid/AoC2018/helper/Patch.java
@@ -0,0 +1,19 @@
+package mobi.reid.AoC2018.helper;
+
+public class Patch {
+
+    public int xOffSet;
+            public int yOffSet;
+            public int xFarEdge;
+            public int yFarEdge;
+
+            Patch(int xo, int yo, int xf, int yf){
+                this.xOffSet = xo;
+                this.yOffSet = yo;
+                this.xFarEdge = xf;
+                this.yFarEdge = yf;
+            }
+
+
+
+}
--- src/mobi/reid/AoC2018/main/InitDay3a.java
+++ src/mobi/reid/AoC2018/main/InitDay3a.java
@@ -1,6 +1,7 @@
 package mobi.reid.AoC2018.main;
 
 import mobi.reid.AoC2018.helper.Helper;
+import mobi.reid.AoC2018.helper.Patch;
 
 
 public class InitDay3a {
@@ -8,8 +9,21 @@ public class InitDay3a {
     public static void main(String[] args) {
         String input = Helper.getStringfromFile("./resources/day2ainput.txt");
 
-        System.out.println(input);
-
+        //System.out.println(input);
+            createPatchfromInput("#17 @ 258,382: 10x26");
     }
+
+
+    public static Patch createPatchfromInput(String input){
+      //#17 @ 258,382: 10x26
+      String offSets = input.substring(input.indexOf("@")+1,input.indexOf(":"));
+      String span    = input.substring(input.indexOf(":")+1);
+      int xOffset = 0;
+        System.out.println(":"+offSets);
+        System.out.println(":"+span);
+        return null;
+    }
+
+
 
 }
